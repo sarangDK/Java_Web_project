2f99661a3b30eaa26f56fc9eaca602b5678c9563
sarangDK <127158524+sarangDK@users.noreply.github.com>
2024-10-30 17:20:26 -0400

    oct.30

--- microservice-parent/booking-service/src/main/java/ca/gbc/bookingservice/repository/BookingRepository.java
+++
@@ -1,7 +0,0 @@
-package ca.gbc.bookingservice.repository;
-
-import ca.gbc.bookingservice.model.Booking;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface BookingRepository extends MongoRepository<Booking, String> {
-}
