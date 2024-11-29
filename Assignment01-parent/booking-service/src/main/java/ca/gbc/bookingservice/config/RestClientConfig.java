10d7dc3c8017274d5b66894fe3d57f030052b02a
sarangDK <127158524+sarangDK@users.noreply.github.com>
2024-11-29 13:26:18 -0500

    Steven - nov 29 / room,booking services updated

---
+++ Assignment01-parent/booking-service/src/main/java/ca/gbc/bookingservice/config/RestClientConfig.java
@@ -0,0 +1,47 @@
+package ca.gbc.bookingservice.config;
+
+
+import ca.gbc.bookingservice.client.RoomServiceClient;
+import org.apache.hc.core5.http.impl.nio.ClientHttp1IOEventHandler;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.client.ClientHttpRequestFactories;
+import org.springframework.boot.web.client.ClientHttpRequestFactorySettings;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.ClientHttpRequestFactory;
+import org.springframework.web.client.RestClient;
+import org.springframework.web.client.support.RestClientAdapter;
+import org.springframework.web.service.invoker.HttpServiceProxyFactory;
+
+import java.time.Duration;
+
+@Configuration
+public class RestClientConfig {
+
+    @Value("${room.service.url}")
+    private String roomServiceUrl;
+
+
+
+    @Bean
+    public RoomServiceClient roomServiceClient() {
+        RestClient restClient = RestClient.builder()
+                .baseUrl(roomServiceUrl)
+                .requestFactory(getClientRequestFactory())
+                .build();
+
+        var restClientAdapter = RestClientAdapter.create(restClient);
+        var httpServiceProxyFactory = HttpServiceProxyFactory.builderFor(restClientAdapter).build();
+        return httpServiceProxyFactory.createClient(RoomServiceClient.class);
+    }
+
+    private ClientHttpRequestFactory getClientRequestFactory() {
+        ClientHttpRequestFactorySettings clientHttpRequestFactorySettings = ClientHttpRequestFactorySettings.DEFAULTS
+                .withConnectTimeout(Duration.ofSeconds(3))
+                .withReadTimeout(Duration.ofSeconds(3));
+        return ClientHttpRequestFactories.get(clientHttpRequestFactorySettings);
+    }
+}
+
+
