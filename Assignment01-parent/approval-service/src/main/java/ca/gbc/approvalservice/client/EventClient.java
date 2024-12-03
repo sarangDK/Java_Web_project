13f4ea7be77810212feb5361e02fc8c1663483ca
biradon <truongchauminhjps15@gmail.com>
2024-12-02 21:18:28 -0500

    Change from OpenFeign to GetExchange

--- Assignment01-parent/approval-service/src/main/java/ca/gbc/approvalservice/client/EventClient.java
+++ Assignment01-parent/approval-service/src/main/java/ca/gbc/approvalservice/client/EventClient.java
@@ -2,16 +2,32 @@ package ca.gbc.approvalservice.client;
 
 
 import ca.gbc.approvalservice.dto.EventResponse;
-import org.springframework.cloud.openfeign.FeignClient;
+
+import groovy.util.logging.Slf4j;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.service.annotation.GetExchange;
 
-@FeignClient(value = "event-service", url = "${event.service.url}")
+
+@Slf4j
 public interface EventClient {
+
+    Logger log = LoggerFactory.getLogger(EventClient.class);
+
+    @GetExchange("api/v1/event/{eventId}")
+    @CircuitBreaker(name = "event", fallbackMethod = "fallbackMethod")
     @RequestMapping(method = RequestMethod.GET, value="/api/v1/event/{eventId}")
     ResponseEntity<EventResponse> getEventById(@PathVariable String eventId);
+
+    default boolean fallbackMethod(String eventName, Throwable throwable) {
+        log.info("Cannot get event for event name {}, failure reason: {}", eventName, throwable.getMessage());
+        return false;
+    }
 }
 
 
--- Assignment01-parent/approval-service/src/main/java/ca/gbc/approvalservice/client/UserClient.java
+++ Assignment01-parent/approval-service/src/main/java/ca/gbc/approvalservice/client/UserClient.java
@@ -1,13 +1,27 @@
 package ca.gbc.approvalservice.client;
 
-import org.springframework.cloud.openfeign.FeignClient;
+import groovy.util.logging.Slf4j;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.service.annotation.GetExchange;
 
-@FeignClient(value = "user-service", url = "${user.service.url}")
+
+
+@Slf4j
 public interface UserClient {
+
+    Logger log = LoggerFactory.getLogger(UserClient.class);
+    @GetExchange("/api/v1/user")
+    @CircuitBreaker(name = "user", fallbackMethod = "fallbackMethod")
     @RequestMapping(method = RequestMethod.GET, value="/api/v1/user/isStaff/{userId}")
     String isStaff(@PathVariable("userId") Long userId);
+
+    default boolean fallbackMethod(String userName, Throwable throwable) {
+        log.info("Cannot get user for user name {}, failure reason: {}", userName, throwable.getMessage());
+        return false;
+    }
 }
